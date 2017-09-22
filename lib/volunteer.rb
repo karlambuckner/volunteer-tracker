c0b8f8e59572df98f854fa0a5db986dd3ba475ae
Karla Buckner <kmb2662@gmail.com>
2017-09-22 13:07:46 -0700

    add more passing specs for volunteers class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -16,4 +16,11 @@ class Volunteer
     volunteers
   end
 
+  def save
+    @id  = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING ID").first['id'].to_i
+  end
+
+  def ==(another_volunteer)
+    self.name.==(another_volunteer.name) and self.id.==(another_volunteer.id) and self.project_id.==(another_volunteer.project_id)
+  end
 end
